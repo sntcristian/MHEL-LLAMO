bad1e3f1424d3f7ed589b14c8b48fb4216a330be
sntcristian <snt.cristian@gmail.com>
2025-08-27 16:22:00 +0200

    minfix

--- execute_slurm.sh
+++ execute_slurm.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=first_job    # Name of your job
+#SBATCH --job-name=filter_and_prompt    # Name of your job
 #SBATCH --output=%x_%j.out            # Output file (%x for job name, %j for job ID)
 #SBATCH --error=%x_%j.err             # Error file
 #SBATCH --time=23:59:59
 #SBATCH --partition=A100 
 #SBATCH --nodes=1
-#SBATCH --gpus=3                   
+#SBATCH --gpus=1                   
 
 # Print job details
 echo "Starting job on node: $(hostname)"
 echo "Job started at: $(date)"
 
 # Define variables for your job
-JSON_FILE="./results/DZ_IT/candidates_test_top50_en.json"
+JSON_FILE="./results/DZ_IT/candidates_test_top50_it.json"
 DATASET_PATH="./test_data/DZ_IT"
 OUTPUT_DIR="./results/DZ_IT/"
-MODEL_ID="mistralai/Mixtral-8x7B-v0.1"
+MODEL_ID="meta-llama/Llama-3.1-8B-Instruct"
+THRESHOLD=20.7
 N_CANDIDATES=20
 HF_TOKEN=""  
 
@@ -25,12 +26,13 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate llm
 
 # Execute the Python script with specific arguments
-srun python prompt_llm.py \
+srun python filter_and_prompt.py \
     --json_f "$JSON_FILE" \
     --dataset_path "$DATASET_PATH" \
     --output_dir "$OUTPUT_DIR" \
     --model_id "$MODEL_ID" \
     --n_candidates $N_CANDIDATES \
+    --threshold $THRESHOLD \
     --hf_token "$HF_TOKEN"
 
 # Print job completion time
